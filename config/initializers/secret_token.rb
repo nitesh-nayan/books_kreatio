dbe1289231e77f362d1525b909362b78d79e21e2
Nitesh Nayan <niteshsingh_26@yahoo.com>
2013-12-10 16:22:41 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BooksKreatio::Application.config.secret_key_base = '05a46d82811a767b100cc5cc5cad88c4de0f053373274ce2478ded46b8b1982b2a6304e33fb91670615144d7932f16e0e8fc2faf161ec11e4ceeaa546a23feec'
